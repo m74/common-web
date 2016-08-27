70b14037ea24503daf42a6323eab4a9f73083fa1
Maxim Smirnov <mixam@h4y.ru>
2016-08-27 17:04:12 +0300

    добавлена совместимость с ресурсами в META-INF/resources

--- dom4web/src/main/java/mixam/dom4web/URIResolver.java
+++ dom4web/src/main/java/mixam/dom4web/URIResolver.java
@@ -1,72 +1,82 @@
 package mixam.dom4web;
 
-import java.io.File;
+import mixam.webtools.Logger;
 
 import javax.servlet.ServletContext;
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamSource;
-
-import mixam.webtools.Logger;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
- * 
  * @author Maxim
  */
 public class URIResolver implements javax.xml.transform.URIResolver {
 
-	private ServletContext ctx;
+    private ServletContext ctx;
 
-	// private String defaultBase;
+    // private String defaultBase;
 
-	public URIResolver(ServletContext ctx) {
-		this.ctx = ctx;
-		// baseDir = ctx.getInitParameter("base");
+    public URIResolver(ServletContext ctx) {
+        this.ctx = ctx;
+        // baseDir = ctx.getInitParameter("base");
 
-		Logger.debug(this, "setup context: " + ctx);
-	}
+        Logger.debug(this, "setup context: " + ctx);
+    }
 
-	@Override
-	public Source resolve(String href, String pageBase) throws TransformerException {
-		Logger.debug(this, "base: " + pageBase);
+    @Override
+    public Source resolve(String href, String pageBase) throws TransformerException {
+        Logger.debug(this, "base: " + pageBase);
 
-		String systemId = href;
-		Logger.debug(this, "href: " + systemId);
+        String systemId = href;
+        Logger.debug(this, "href: " + systemId);
 
-		if (pageBase != null) {
+        if (pageBase != null) {
 
-			if (href.charAt(0) != '/') {
+            if (href.charAt(0) != '/') {
 
-				// Отбрасываем файл и получаем текущую директорию
-				if (pageBase.charAt(pageBase.length() - 1) != '/')
-					pageBase = pageBase.substring(0, pageBase.lastIndexOf('/') + 1);
+                // Отбрасываем файл и получаем текущую директорию
+                if (pageBase.charAt(pageBase.length() - 1) != '/')
+                    pageBase = pageBase.substring(0, pageBase.lastIndexOf('/') + 1);
 
-				systemId = pageBase + href;
-			}
+                systemId = pageBase + href;
+            }
 
-			Logger.debug(this, "systemId: " + systemId);
+            Logger.debug(this, "systemId: " + systemId);
 
-			// Преобразовать отностельный путь в абсолютный
-			if (systemId.charAt(0) == '/') {
-				// Убрать contextPath из systemId, иначе getRealPath() сработает
-				// не корректно.
-				// !!! Непонятно зачем убирать, может изначально указывать путь
-				// с учетом контекста
-				// systemId = systemId.replaceFirst(
-				// "/" + ctx.getServletContextName(), "");
+            // Преобразовать отностельный путь в абсолютный
+            if (systemId.charAt(0) == '/') {
+                // Убрать contextPath из systemId, иначе getRealPath() сработает
+                // не корректно.
+                // !!! Непонятно зачем убирать, может изначально указывать путь
+                // с учетом контекста
+                // systemId = systemId.replaceFirst(
+                // "/" + ctx.getServletContextName(), "");
 
-				File f = new File(ctx.getRealPath(systemId));
-				systemId = "file:" + f.getAbsolutePath();
-			}
-		}
+                File f = new File(ctx.getRealPath(systemId));
+                if (f.exists())
+                    systemId = "file:" + f.getAbsolutePath();
+                else {
+                    try {
+                        URL url = ctx.getResource(systemId);
+                        if (url == null) throw new TransformerException("Resource not foubd: " + systemId);
+                        systemId = url.toString();
+                    } catch (MalformedURLException e) {
+                        throw new TransformerException(e);
+                    }
+                }
+            }
+        }
 
-		Logger.debug(this, "resolve: " + systemId);
+        Logger.debug(this, "resolve: " + systemId);
 
-		return new StreamSource(systemId);
-	}
+        return new StreamSource(systemId);
+    }
 
-	// public void setDefaultBase(String defaultBase) {
-	// this.defaultBase = defaultBase;
-	// Logger.debug(this, "defaultBase: " + defaultBase);
-	// }
+    // public void setDefaultBase(String defaultBase) {
+    // this.defaultBase = defaultBase;
+    // Logger.debug(this, "defaultBase: " + defaultBase);
+    // }
 }
